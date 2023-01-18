b02a628e6517abb02ac799c10ad10433de397758
Alex Ngan <a.ngan@hotmail.co.uk>
2023-01-18 21:56:39 +0000

    created auth page. created link to back end firebase.

--- my-app/src/components/Header/Header.js
+++ my-app/src/components/Header/Header.js
@@ -1,24 +1,37 @@
 import React from 'react'
 import './header.css'
 import { FaHome } from "react-icons/fa";
-import {Link,useNavigate} from 'react-router-dom'
+import { Link,useNavigate } from 'react-router-dom'
+import { auth } from '../../config/firebaseConfig'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { signOut } from 'firebase/auth';
 
 
 function Header() {
 
     const categories = ["Health","Food","Travel","Technology"]
     let navigate=useNavigate(); 
+    const [user]=useAuthState(auth)
 
   return (
     <div className='header-container'>
-        <FaHome className='home-icon' 
-        onClick={()=>navigate("/")}/>
+        <FaHome className='home-icon' onClick={()=>navigate("/")}/>
+        <div className='categories-container'>
+          {
+              categories.map(item=>{
+                  return <Link className='nav-link' to={`/categories/${item}`}>{item}</Link>
+              })
+          }
+        </div>
         {
-            categories.map(item=>{
-                return <Link className='nav-link' to={`/categories/${item}`}>{item}</Link>
-            })
+          user
+          ? <div>
+            <span className='username '>{user?.displayName ? user?.displayName : user?.email }</span>
+            <button onClick={()=>signOut(auth)} className='auth-link' >Logout</button>
+          </div>
+          : <Link className='auth-link' to={`/auth`}>SignUp</Link>
+
         }
-        <Link className='auth-link' to={`/auth`}>SignUp</Link>
     </div>
   )
 }
