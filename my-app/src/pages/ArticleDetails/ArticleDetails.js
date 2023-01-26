d1cb0a9c4c25d137cb15fec01c44aa30f9fac662
Alex Ngan <a.ngan@hotmail.co.uk>
2023-01-26 17:23:29 +0000

    created more code in the caricle page. made the like button functional

--- my-app/src/pages/ArticleDetails/ArticleDetails.js
+++ my-app/src/pages/ArticleDetails/ArticleDetails.js
@@ -1,5 +1,10 @@
 import React, { useState,useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import { db } from '../../config/firebaseConfig'
+import { getDoc,doc } from 'firebase/firestore'
+import './articleDetails.css'
+import Likes from '../../components/Likes/Likes'
+
 
 function ArticleDetails() {
 
@@ -7,11 +12,32 @@ function ArticleDetails() {
   const [article,setArticle]=useState('')
 
   useEffect(() => {
-    const docRef =doc(db,"articles",articleId)
+    const docRef=doc(db,"articles",articleId)
+    getDoc(docRef)
+    .then(res=>{
+      setArticle(res.data())
+    })
+    .catch(err=>console.log(err))
   }, [])
   
   return (
-  <div>{articleId}</div>
+  <div>
+    <div className="details-container">
+        <h1>{article?.title}</h1>
+        <h2>{article?.summary}</h2> 
+        <div className="details-info-container">
+          <p><span className="article-span">Author:</span> {article?.createdBy?.toUpperCase()}</p>
+          <p><span className="article-span published">Published:</span> {article?.createdAt?.toDate().toDateString()}</p>
+          <Likes articleId={articleId}/>
+        </div>
+        <div style={{borderBottom:"solid 1px grey",paddingBottom:"10px",marginBottom:"10px"}}>
+          <img className="details-image" src={article?.imageUrl}/> 
+          <p className="article-description">{article?.paragraphOne}</p>
+          <p className="article-description">{article?.paragraphTwo}</p>
+          <p className="article-description">{article?.paragraphThree}</p>
+        </div>
+    </div>
+  </div>
   )
 }
 
